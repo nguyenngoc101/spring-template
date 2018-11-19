7295c1705ce248cf97db0f89dab4b17635db37f3
nguyenngoc101 <nguyenngoc101@yahoo.com>
2018-11-20 00:33:43 +0700

    remove unused imports

--- src/main/java/com/ngocnv/springtemplate/controller/UserController.java
+++ src/main/java/com/ngocnv/springtemplate/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Size;
 import java.util.Collections;
 import java.util.List;
 
